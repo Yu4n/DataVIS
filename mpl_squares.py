1b36f289df19b3b081bd4d2c989f8190e8356287
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-03 19:26:59 +0800

    update

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
+import math
 
-squares = [x * x for x in range(1, 6)]
+x_values = range(1, 1000)
+y_values = [x * x for x in x_values]
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Greens, s=10)
+# ax.plot(input_values, squares, linewidth=3)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -12,4 +16,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # Set size of tick labels.
 ax.tick_params(axis='both', labelsize=14)
 
+# Set the range for each axis.
+# ax.axis([0, 101, 0, 10000])
+
+# plt.savefig('C:\\Users\\username\\Desktop\\squares_plot.png')
 plt.show()
