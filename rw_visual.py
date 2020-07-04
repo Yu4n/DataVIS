6c0d3dc217bfe798c46eb4c26cb99d3d9bef6dd1
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-04 20:29:45 +0800

    1.Add starting and ending points. 2.Adding Plot Points. 3.Remove axes.(optional)

--- rw_visual.py
+++ rw_visual.py
@@ -4,19 +4,27 @@ from random_walk import RandomWalk
 
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     # Plot the points in the walk.
-    plt.style.use('seaborn-whitegrid')
+    plt.style.use('seaborn-white')
     # print(plt.style.available)
     fig, ax = plt.subplots()
-    point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors=None, s=10)
+    point_numbers = range(rw.num_points)  # Starting point
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors=None, s=1)  # Ending point
 
     # Emphasize the first and last points.
-    
+    ax.scatter(0, 0, c='green', edgecolors=None, s=10)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors=None, s=10)
     # plt.savefig('random_walk.png')
+
+    '''
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    '''
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
