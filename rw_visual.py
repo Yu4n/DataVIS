084ed2412773d1f5d1451caf58406b0c5ed985a1
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-04 20:47:36 +0800

    update

--- rw_visual.py
+++ rw_visual.py
@@ -4,19 +4,19 @@ from random_walk import RandomWalk
 
 while True:
     # Make a random walk.
-    rw = RandomWalk(50000)
+    rw = RandomWalk(5000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('seaborn-white')
     # print(plt.style.available)
     fig, ax = plt.subplots()
-    point_numbers = range(rw.num_points)  # Starting point
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors=None, s=1)  # Ending point
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors=None, s=10)
 
     # Emphasize the first and last points.
-    ax.scatter(0, 0, c='green', edgecolors=None, s=10)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors=None, s=10)
+    ax.scatter(0, 0, c='green', edgecolors=None, s=100)  # Starting point
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors=None, s=100)  # Ending point
     # plt.savefig('random_walk.png')
 
     '''
