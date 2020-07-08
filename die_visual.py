9bdea3e3625c25fc93ba8f524d72d3bb55822b4d
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-08 17:27:54 +0800

    add list comprehension and d8_d8.html.

--- die_visual.py
+++ die_visual.py
@@ -4,23 +4,17 @@ from plotly import offline
 
 
 # Create two D6 Dice.
-die_1 = Die()
-die_2 = Die()
+die_1 = Die(8)
+die_2 = Die(8)
 
 # Make some rolls, and store results in a list.
-results = []
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for roll_num in range(1000)]
 
 # print(results)
 
 # Analyze the results.
-frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(2, max_result + 1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(2, max_result + 1)]
 
 # print(frequencies)
 
@@ -31,4 +25,4 @@ data = [Bar(x=x_values, y=frequencies)]
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 my_layout = Layout(title='Results of rolling two D6 dice 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d8_d8.html')
