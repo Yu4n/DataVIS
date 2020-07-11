f87e0caaccb37750a62ee0c99d5d44099c9979bf
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-11 16:44:33 +0800

    Shade of sitka_highs_and_lows_2018.png added.

--- sitka_highs.py
+++ sitka_highs.py
@@ -24,6 +24,7 @@ plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
 ax.plot(dates, lows, c='blue')
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
 plt.title("Daily high and low temperatures - 2018", fontsize=24)
@@ -31,6 +32,6 @@ plt.xlabel('', fontsize=16)
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='minor', labelsize=16)
 fig.autofmt_xdate()
-plt.savefig('sitka_highs_and_lows_2018.png')
+# plt.savefig('sitka_highs_and_lows_2018.png')
 
 plt.show()
