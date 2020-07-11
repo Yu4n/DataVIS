e638cd2674db0a4e3560a5862aca0bfe40bc96c0
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-11 16:03:55 +0800

    dates added,

--- sitka_highs.py
+++ sitka_highs.py
@@ -7,20 +7,27 @@ filename = './data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    highs = []
+
+    # Get dates and high temperature from this file.
+    dates, highs = [], []
+
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
 # plot high temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs)
+ax.plot(dates, highs, c='red')
 
 # Format plot.
 plt.title("Daily high temperatures, July 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 plt.ylabel("Temperature (F)", fontsize=16)
-plt.tick_params(axis='both', which='both', labelsize=16)
+plt.tick_params(axis='both', which='minor', labelsize=16)
+fig.autofmt_xdate()
+# plt.savefig('sitka_highs.png')
 
 plt.show()
