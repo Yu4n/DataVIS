11f45487fe6ab4b850ab0356a372c3d2d0ac0bd8
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-19 16:16:50 +0800

    Build a world map global_earthquakes.html

--- eq_explore _data.py
+++ eq_explore _data.py
@@ -9,7 +9,6 @@ with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
 
 mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
@@ -20,6 +19,9 @@ for eq_dict in all_eq_dicts:
     lats.append(lat)
     mags.append(mag)
 
-print(mags[:10])
-print(lons[:5])
-print(lats[:5])
+# Map the earthquakes.
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
