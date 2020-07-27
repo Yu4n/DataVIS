05fcd46d28e7145014ff0504ae360ea454effff2
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-27 16:14:49 +0800

    Generate github_stars.json.

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from plotly.graph_objs import Bar
 from plotly import offline
 
@@ -11,39 +12,5 @@ print(f"Status code: {r.status_code}")
 
 # Process results.
 response_dict = r.json()
-repo_dicts = response_dict['items']
-repo_names, stars = [], []
-for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
-
-# Make visualization.
-data = [{
-    'type': 'bar',
-    'x': repo_names,
-    'y': stars,
-    'marker': {
-        'color': 'rgb(255, 0, 0)',
-        'line': {'width': 1.5, 'color': 'rgb(60, 100, 150)'}
-    },
-    'opacity': 0.6,
-}]
-
-my_layout = {
-    'title': 'Most-Starred Python Projects on GitHub',
-    'titlefont': {'size': 28},
-    'title_x': 0.5,
-    'xaxis': {
-        'title': 'Repository',
-        'titlefont': {'size': 24},
-        'tickfont': {'size': 14},
-    },
-    'yaxis': {
-        'title': 'Stars',
-        'titlefont': {'size': 24},
-        'tickfont': {'size': 14},
-    },
-}
-
-fig = {'data': data, 'layout': my_layout}
-offline.plot(fig, filename='python_repos.html')
+with open('./data/github_stars.json', 'w') as f:
+    json.dump(response_dict, f, indent=4)
