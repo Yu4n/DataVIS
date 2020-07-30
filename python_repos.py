7a5e03390948a52eb82ad9880ecadbc94fd848bb
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-07-30 10:53:36 +0800

    Add hovertext.

--- python_repos.py
+++ python_repos.py
@@ -1,5 +1,4 @@
 import requests
-import json
 from plotly.graph_objs import Bar
 from plotly import offline
 
@@ -12,5 +11,44 @@ print(f"Status code: {r.status_code}")
 
 # Process results.
 response_dict = r.json()
-with open('./data/github_stars.json', 'w') as f:
-    json.dump(response_dict, f, indent=4)
+repo_dicts = response_dict['items']
+repo_names, stars, labels = [], [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+
+# Make visualization.
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+    'hovertext': labels,
+    'marker': {
+        'color': 'rgb(255, 0, 0)',
+        'line': {'width': 1.5, 'color': 'rgb(60, 100, 150)'}
+    },
+    'opacity': 0.6,
+}]
+
+my_layout = {
+    'title': 'Most-Starred Python Projects on GitHub',
+    'titlefont': {'size': 28},
+    'title_x': 0.5,
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
